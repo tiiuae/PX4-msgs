89c18b1e73efd6f26853ffc5d50bfb0142f2e814
Jari Nippula <jarix@ssrc.tii.ae>
2022-12-13 14:25:40 +0200

    CI scripts
    Files for create debian package using docker env.
    Github action to build and push deb into artifactory

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+output_dir=$1
+build_number=${GITHUB_RUN_NUMBER:=0}
+
+docker build \
+    --build-arg BUILD_NUMBER=${build_number} \
+    -t "tii-px4-msgs:px4-msgs" .
+
+container_id=$(docker create tii-px4-msgs:px4-msgs)
+docker cp ${container_id}:/output_dir/. ${output_dir}
+docker rm -v ${container_id}
+
+exit 0
